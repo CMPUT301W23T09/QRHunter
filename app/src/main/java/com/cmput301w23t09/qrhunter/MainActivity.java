5211cb1a65648a491bece7562a9a97953be4406a
John Mabanta <johnmabanta51@gmail.com>
2023-02-16 09:59:05 -0700

    Initial codebase

---
+++ app/src/main/java/com/cmput301w23t09/qrhunter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cmput301w23t09.qrhunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
