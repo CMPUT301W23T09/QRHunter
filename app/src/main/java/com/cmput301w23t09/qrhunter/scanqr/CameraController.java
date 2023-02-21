c8ac50c4458b896c650107b867446e05be2fedd7
John Mabanta <johnmabanta51@gmail.com>
2023-02-21 00:49:37 -0700

    Got camera preview

--- app/src/main/java/com/cmput301w23t09/qrhunter/scanqr/CameraController.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/scanqr/CameraController.java
@@ -1,17 +1,26 @@
 package com.cmput301w23t09.qrhunter.scanqr;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Manages Camera preview and capture using Google's CameraX library
  * https://developer.android.com/training/camerax
+ * (Used ChatGPT to convert Kotlin to Java)
  *
  * @author John Mabanta
  * @version 1.0
@@ -24,30 +33,64 @@ public class CameraController {
 
     private static final String TAG = "QRHunterCamera";
     private static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
-    private static final String[] REQUIRED_PERMISSIONS = new String[] {
+    public static final int REQUEST_CODE_PERMISSIONS = 10;
+    public static final String[] REQUIRED_PERMISSIONS = new String[]{
             Manifest.permission.CAMERA
     };
 
     public CameraController(ScannerFragment scannerFragment) {
         this.scannerFragment = scannerFragment;
+        if (!allPermissionsGranted())
+            scannerFragment.requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        cameraExecutor = Executors.newSingleThreadExecutor();
     }
 
-    private boolean checkPermissionsGranted() {
-        for (String permission: REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(scannerFragment.getContext(), permission) == PackageManager.PERMISSION_DENIED)
-                return false;
-        }
-        return true;
-    }
+    /**
+     * Starts the camera and binds it's preview to the PreviewView UI element.
+     *
+     * @param previewView The PreviewView UI element to show the camera preview on.
+     */
+    public void startCamera(PreviewView previewView) {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture
+                = ProcessCameraProvider.getInstance(scannerFragment.getContext());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
-    private void requestPermissions() {
-        scannerFragment.requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+                // Attach preview screen
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                // Use back camera
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                // Bind camera
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle((LifecycleOwner) scannerFragment, cameraSelector, preview);
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }, ContextCompat.getMainExecutor(scannerFragment.getContext()));
     }
 
     public void onDestroy() {
         cameraExecutor.shutdown();
     }
 
+    /**
+     * Checks if all required permissions are granted
+     *
+     * @return True if every permission under CameraController.REQUIRED_PERMISSIONS is granted
+     */
+    public boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(scannerFragment.getContext(), permission)
+                    == PackageManager.PERMISSION_DENIED)
+                return false;
+        }
+        return true;
+    }
 
 }
--- app/src/main/java/com/cmput301w23t09/qrhunter/scanqr/ScannerFragment.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/scanqr/ScannerFragment.java
@@ -4,18 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.cmput301w23t09.qrhunter.BaseFragment;
 import com.cmput301w23t09.qrhunter.MainController;
-import com.cmput301w23t09.qrhunter.R;
 import com.cmput301w23t09.qrhunter.databinding.FragmentScanqrBinding;
 
 public class ScannerFragment extends BaseFragment {
 
     private ScannerController scannerController;
+    private CameraController cameraController;
     private FragmentScanqrBinding binding;
 
     public ScannerFragment(MainController mainController) {
@@ -24,9 +25,24 @@ public class ScannerFragment extends BaseFragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         scannerController = new ScannerController();
+        cameraController = new CameraController(this);
         binding = FragmentScanqrBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == CameraController.REQUEST_CODE_PERMISSIONS) {
+            if (cameraController.allPermissionsGranted()) {
+                cameraController.startCamera(binding.viewFinder);
+            } else {
+                Toast.makeText(getContext(), "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
