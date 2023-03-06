da82bdc8652d53abcb7c0e3fa01b4910b822523d
Frances <francesiigwe@gmail.com>
2023-03-06 16:48:59 -0700

    added add and delete buttons, still in working progress

--- app/src/main/java/com/cmput301w23t09/qrhunter/qrcode/QRCodeFragment.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/qrcode/QRCodeFragment.java
@@ -1,8 +1,11 @@
 package com.cmput301w23t09.qrhunter.qrcode;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -10,16 +13,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import com.cmput301w23t09.qrhunter.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRCodeFragment extends DialogFragment {
 
   // TODO: Figure out how to know when to display Add (+) button or Delete (Trash) button
   private TextView qrName;
+  private FloatingActionButton addButton;
+  private FloatingActionButton deleteButton;
 
   /**
    * Creates a new QRCodeFragment to display a specific QR Code
    *
-   * <p>TODO: Replase hash with QRCode object
+   * <p>TODO: Replace hash with QRCode object
    *
    * @param hash Hash of the QR code to view
    * @return
@@ -46,8 +62,78 @@ public class QRCodeFragment extends DialogFragment {
    */
   private void setupViews(View view) {
     qrName = view.findViewById(R.id.qrName);
+
     qrName.setText((String) getArguments().get("hash"));
+
+    /**----------------------------------------------------------------**/
+    String hash = getArguments().getString("hash");
+
+
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    DocumentReference docRef = database.collection("qrcodes").document(hash);
+
+
+    addButton = view.findViewById(R.id.addButton);
+    deleteButton = view.findViewById(R.id.deleteButton);
+
+    Task<DocumentSnapshot> task = docRef.get();
+
+    if (task.isSuccessful()) {
+      DocumentSnapshot document = task.getResult();
+      if (document.exists()) {
+        addButton.setVisibility(View.GONE);
+        deleteButton.setVisibility(View.VISIBLE);
+        deleteButton.setOnClickListener(v -> {
+          // TODO: Delete the QR Code from Firestore
+          database.collection("qrcodes").document(hash)
+                  .delete()
+                  .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                      Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                  })
+                  .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                      Log.w(TAG, "Error deleting document", e);
+                    }
+                  });
+
+          dismiss();
+        });
+      } else {
+        addButton.setVisibility(View.VISIBLE);
+        deleteButton.setVisibility(View.GONE);
+        addButton.setOnClickListener(v -> {
+          // TODO: Add the QR Code to Firestore
+
+          Map<String, Object> data = new HashMap<>();
+          data.put("hash", hash);
+          database.collection("qrcodes").document(hash)
+                  .set(data)
+                  .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                      Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                  })
+                  .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                      Log.w(TAG, "Error writing document", e);
+                    }
+                  });
+
+          dismiss();
+        });
+      }
+    } else {
+      Log.d(TAG, "get failed with ", task.getException());
+    }
   }
+
+
 
   /**
    * Creates a dialog box to display QRCode information in
