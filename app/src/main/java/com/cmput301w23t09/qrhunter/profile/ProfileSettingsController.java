38f9321e745aff08f3f42bae19bfd39d652f8297
WillQi <williamqipizza@gmail.com>
2023-03-09 13:49:44 -0700

    Profile settings page

--- app/src/main/java/com/cmput301w23t09/qrhunter/profile/ProfileSettingsController.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/profile/ProfileSettingsController.java
@@ -1,6 +1,11 @@
 package com.cmput301w23t09.qrhunter.profile;
 
 import com.cmput301w23t09.qrhunter.GameController;
+import com.cmput301w23t09.qrhunter.player.Player;
+import com.cmput301w23t09.qrhunter.player.PlayerDatabase;
+import com.cmput301w23t09.qrhunter.util.DeviceUtils;
+import com.cmput301w23t09.qrhunter.util.ValidationUtils;
+import java.util.function.Consumer;
 
 public class ProfileSettingsController {
 
@@ -10,7 +15,64 @@ public class ProfileSettingsController {
     this.gameController = gameController;
   }
 
+  /**
+   * Saves the changes of the player's new contact details. It is assumed that the email and phone
+   * number are valid.
+   *
+   * @param email the new email
+   * @param phoneNo the new phone number
+   * @param callback callback to call on completion or failure. True if completed successfully.
+   */
+  public void saveChanges(String email, String phoneNo, Consumer<Boolean> callback) {
+    assert ValidationUtils.isValidEmail(email);
+    assert ValidationUtils.isValidPhoneNo(phoneNo);
+
+    requestPlayerData(
+        player -> {
+          if (player == null) {
+            callback.accept(false);
+            return;
+          }
+
+          // Update player details
+          player.setEmail(email);
+          player.setPhoneNo(phoneNo);
+
+          PlayerDatabase.getInstance()
+              .update(
+                  player,
+                  task -> {
+                    if (task.getException() != null) {
+                      callback.accept(false);
+                      return;
+                    }
+
+                    callback.accept(true);
+                  });
+        });
+  }
+
+  /** Returns to the profile fragment */
   public void returnToProfile() {
     gameController.setBody(new ProfileFragment(gameController));
+  }
+
+  /**
+   * Request the player of this settings page.
+   *
+   * @param callback callback to call with the player or null if an exception occurred while
+   *     fetching the player.
+   */
+  public void requestPlayerData(Consumer<Player> callback) {
+    PlayerDatabase.getInstance()
+        .getPlayerByDeviceId(
+            DeviceUtils.getDeviceUUID(gameController.getActivity()),
+            task -> {
+              if (task.getException() != null) {
+                callback.accept(null);
+              } else {
+                callback.accept(task.getData());
+              }
+            });
   }
 }
--- app/src/main/java/com/cmput301w23t09/qrhunter/profile/ProfileSettingsFragment.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/profile/ProfileSettingsFragment.java
@@ -1,17 +1,28 @@
 package com.cmput301w23t09.qrhunter.profile;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.cmput301w23t09.qrhunter.BaseFragment;
 import com.cmput301w23t09.qrhunter.GameController;
 import com.cmput301w23t09.qrhunter.R;
+import com.cmput301w23t09.qrhunter.util.ValidationUtils;
+import com.google.android.material.textfield.TextInputEditText;
 
+/** Fragment to display and edit contact details */
 public class ProfileSettingsFragment extends BaseFragment {
   private ProfileSettingsController controller;
+
+  private TextInputEditText phoneField;
+  private TextInputEditText emailField;
+
+  private String currentSavedEmail;
+  private String currentSavedPhoneNo;
 
   public ProfileSettingsFragment(GameController gameController) {
     super(gameController);
@@ -26,9 +37,100 @@ public class ProfileSettingsFragment extends BaseFragment {
     controller = new ProfileSettingsController(getMainController());
     View view = inflater.inflate(R.layout.fragment_profile_settings, container, false);
 
-    view.findViewById(R.id.settings_back_button)
-        .setOnClickListener(v -> controller.returnToProfile());
+    phoneField = view.findViewById(R.id.settings_screen_phoneTextField);
+    emailField = view.findViewById(R.id.settings_screen_emailTextField);
+
+    view.findViewById(R.id.settings_back_button).setOnClickListener(v -> this.onBackClick());
+    view.findViewById(R.id.settings_reset_details_button)
+        .setOnClickListener(v -> this.onResetClick());
+    view.findViewById(R.id.settings_save_button).setOnClickListener(v -> this.onSaveClick());
+
+    loadPlayerContactDetails();
 
     return view;
+  }
+
+  /** Handles logic when the back button is clicked. */
+  private void onBackClick() {
+    boolean dataLoaded = currentSavedEmail != null && currentSavedPhoneNo != null;
+    boolean changesMade =
+        dataLoaded
+            && (!phoneField.getText().toString().equals(currentSavedPhoneNo)
+                || !emailField.getText().toString().equals(currentSavedEmail));
+
+    if (!changesMade) {
+      controller.returnToProfile();
+      return;
+    }
+
+    // Show confirm dialog
+    View view =
+        getLayoutInflater()
+            .inflate(R.layout.fragment_unsaved_changes_prompt, (ViewGroup) getView(), false);
+    new AlertDialog.Builder(getContext())
+        .setView(view)
+        .setPositiveButton("Leave", (dia, l) -> controller.returnToProfile())
+        .setNegativeButton("Go Back", (dia, l) -> {})
+        .show();
+  }
+
+  /** Handles logic when the reset button is clicked. */
+  private void onResetClick() {
+    phoneField.setText("");
+    emailField.setText("");
+  }
+
+  /** Handles logic when the save button is clicked. */
+  private void onSaveClick() {
+    String phoneNo = phoneField.getText().toString();
+    String email = emailField.getText().toString();
+
+    if (!ValidationUtils.isValidPhoneNo(phoneNo)) {
+      Toast.makeText(getContext(), "That is not a valid phone number", Toast.LENGTH_SHORT).show();
+      return;
+    }
+    if (!ValidationUtils.isValidEmail(email)) {
+      Toast.makeText(getContext(), "That is not a valid email", Toast.LENGTH_SHORT).show();
+      return;
+    }
+
+    controller.saveChanges(
+        email,
+        phoneNo,
+        success -> {
+          if (success) {
+            currentSavedEmail = email;
+            currentSavedPhoneNo = phoneNo;
+
+            Toast.makeText(getContext(), "Saved!", Toast.LENGTH_SHORT).show();
+          } else {
+            Toast.makeText(
+                    getContext(),
+                    "An exception occurred while trying to save your changes.",
+                    Toast.LENGTH_SHORT)
+                .show();
+          }
+        });
+  }
+
+  /** Load the player contact details. */
+  private void loadPlayerContactDetails() {
+    controller.requestPlayerData(
+        player -> {
+          if (player == null) {
+            Toast.makeText(
+                    getContext(),
+                    "An exception occurred while fetching your data.",
+                    Toast.LENGTH_SHORT)
+                .show();
+            return;
+          }
+
+          currentSavedPhoneNo = player.getPhoneNo();
+          phoneField.setText(player.getPhoneNo());
+
+          currentSavedEmail = player.getEmail();
+          emailField.setText(player.getEmail());
+        });
   }
 }
