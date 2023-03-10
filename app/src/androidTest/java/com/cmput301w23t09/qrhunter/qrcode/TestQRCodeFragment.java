4275b4f1b46a3762b69cfac4f974e813a6db7860
John Mabanta <johnmabanta51@gmail.com>
2023-03-10 07:35:19 -0700

    Trying inRoot(isDialog())

--- app/src/androidTest/java/com/cmput301w23t09/qrhunter/qrcode/TestQRCodeFragment.java
+++ app/src/androidTest/java/com/cmput301w23t09/qrhunter/qrcode/TestQRCodeFragment.java
@@ -3,6 +3,7 @@ package com.cmput301w23t09.qrhunter.qrcode;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.awaitility.Awaitility.await;
@@ -49,35 +50,35 @@ public class TestQRCodeFragment {
   @Test
   public void testQRNameDisplay() {
     // TODO: Currently, QRCodeFragment shows hash, CHANGE THIS TO NAME ONCE IMPLEMENTED
-    onView(withId(R.id.qr_name)).check(matches(withText("test-hash123")));
+    onView(withId(R.id.qr_name)).inRoot(isDialog()).check(matches(withText("test-hash123")));
   }
 
   @Test
   public void testQRSetLocation() {
-    onView(withId(R.id.location_request_box)).perform(click());
+    onView(withId(R.id.location_request_box)).inRoot(isDialog()).perform(click());
     await().atMost(30, TimeUnit.SECONDS).until(() -> qrCode.getLoc() != null);
   }
 
   @Test
   public void testQRRemoveLocation() {
-    onView(withId(R.id.location_request_box)).perform(click());
-    onView(withId(R.id.location_request_box)).perform(click());
+    onView(withId(R.id.location_request_box)).inRoot(isDialog()).perform(click());
+    onView(withId(R.id.location_request_box)).inRoot(isDialog()).perform(click());
     await().atMost(30, TimeUnit.SECONDS).until(() -> qrCode.getLoc() == null);
   }
 
   @Test
   public void testSnapLocationPhoto() {
     assertEquals(0, qrCode.getPhotos().size());
-    onView(withId(R.id.take_location_photo_btn)).perform(click());
-    onView(withId(R.id.location_photo_shutter)).perform(click());
+    onView(withId(R.id.take_location_photo_btn)).inRoot(isDialog()).perform(click());
+    onView(withId(R.id.location_photo_shutter)).inRoot(isDialog()).perform(click());
     await().atMost(30, TimeUnit.SECONDS).until(() -> qrCode.getPhotos().size() > 0);
   }
 
   @Test
   public void testRemoveLocationPhoto() {
-    onView(withId(R.id.take_location_photo_btn)).perform(click());
-    onView(withId(R.id.location_photo_shutter)).perform(click());
-    onView(withId(R.id.take_location_photo_btn)).perform(click());
+    onView(withId(R.id.take_location_photo_btn)).inRoot(isDialog()).perform(click());
+    onView(withId(R.id.location_photo_shutter)).inRoot(isDialog()).perform(click());
+    onView(withId(R.id.take_location_photo_btn)).inRoot(isDialog()).perform(click());
     await().atMost(30, TimeUnit.SECONDS).until(() -> qrCode.getPhotos().size() == 0);
   }
 }
