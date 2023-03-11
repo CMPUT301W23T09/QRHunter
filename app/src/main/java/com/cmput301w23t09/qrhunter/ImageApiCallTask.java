2f774b5af3024d6b874de7ec9f9cb4dc9c8fd30d
Andy N <andynguyen9001@gmail.com>
2023-03-11 01:59:55 -0700

    Add basic functionallity for DiceBear api call

---
+++ app/src/main/java/com/cmput301w23t09/qrhunter/ImageApiCallTask.java
@@ -0,0 +1,55 @@
+package com.cmput301w23t09.qrhunter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+// Citation: API Call information - https://www.java67.com/2019/03/7-examples-of-httpurlconnection-in-java.html
+public class ImageApiCallTask extends AsyncTask<Void, Void, Bitmap> {
+    int responseCode;
+    @Override
+    protected Bitmap doInBackground(Void... params) {
+        try {
+            // Open a connection to the image API endpoint
+            URL url = new URL("https://api.dicebear.com/5.x/pixel-art/svg?seed=Bob");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // Set any required headers or parameters
+//            conn.setRequestProperty("Authorization", "Bearer my-auth-token");
+
+            // Get the response code and response body as an InputStream
+            responseCode = conn.getResponseCode();
+            InputStream inputStream;
+            if (responseCode < HttpURLConnection.HTTP_BAD_REQUEST) {
+                inputStream = conn.getInputStream();
+            } else {
+                inputStream = conn.getErrorStream();
+            }
+
+            // Convert the InputStream to a Bitmap
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        // Update the UI with the image data
+        if (result != null) {
+            // Display the image in an ImageView
+            System.out.println("Response code: " + responseCode);
+        } else {
+            // Display an error message
+            Log.d("imageApiCall", "API call for the DiceBear failed.");
+        }
+    }
+}
--- app/src/main/java/com/cmput301w23t09/qrhunter/TempQRCodeClass.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/TempQRCodeClass.java
@@ -1,12 +1,21 @@
 package com.cmput301w23t09.qrhunter;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TempQRCodeClass {
     String qrCodeHash = "696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6";
@@ -33,43 +42,8 @@ public class TempQRCodeClass {
         name = nameBitOne + " " + nameBitTwo + " " + " " + nameBitThree + " " + nameBitFour + " " + nameBitFive + " " + nameBitSix;
     }
 
-    // Refactor: Remove hard-coded values
-    // Citation on how canvas works: https://stackoverflow.com/questions/17954596/how-to-draw-circle-by-canvas-in-android#:~:text=If%20you%20are%20using%20your,canvas%20to%20draw%20a%20circle.
-    // citation on how to save canvas as png: https://stackoverflow.com/questions/13533471/how-to-save-view-from-canvas-to-png-file
     private void createImage() {
-        String binary = getFirstSixBits();
-        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint();
-        Paint paint2 = new Paint();
-        Paint paint3 = new Paint();
-
-        paint.setColor((binary.charAt(0) == '0') ? Color.RED : Color.BLUE);
-        canvas.drawColor((binary.charAt(1) == '0') ? Color.YELLOW : Color.GREEN);
-
-        if (binary.charAt(2) == '0') {
-            canvas.drawCircle(50, 50, 20, paint);
-        } else {
-            canvas.drawRect(10, 10, 90, 90, paint);
-        }
-
-        paint2.setColor((binary.charAt(3) == '0') ? Color.rgb(238,130,238) : Color.rgb(64,224,208));
-
-        if (binary.charAt(4) == '0') {
-            canvas.drawCircle(50, 50, 10, paint2);
-        } else {
-            canvas.drawRect(20, 20, 80, 80, paint2);
-        }
-
-        paint3.setColor(Color.rgb(255,105,180));
-
-        if (binary.charAt(5) == '0') {
-            canvas.drawCircle(50, 50, 10, paint3);
-        } else {
-            canvas.drawRect(20, 20, 80, 80, paint3);
-        }
-
-        saveImage(bitmap);
+        new ImageApiCallTask().execute();
     }
 
     private void saveImage(Bitmap bitmap) {
