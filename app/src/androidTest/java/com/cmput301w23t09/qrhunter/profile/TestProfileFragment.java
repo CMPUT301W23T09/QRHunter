6293aa76c8375a6ec4de5bbe683c3d192d0cdec0
Irene Sun <93227340+IreneRSun@users.noreply.github.com>
2023-03-11 21:30:56 -0700

    Added initial tests for some classes, require pull request #40 to be merged

--- app/src/androidTest/java/com/cmput301w23t09/qrhunter/profile/TestProfileFragment.java
+++ app/src/androidTest/java/com/cmput301w23t09/qrhunter/profile/TestProfileFragment.java
@@ -1,13 +1,21 @@
 package com.cmput301w23t09.qrhunter.profile;
 
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
 import com.cmput301w23t09.qrhunter.GameActivity;
 import com.cmput301w23t09.qrhunter.GameController;
+import com.cmput301w23t09.qrhunter.R;
+import com.robotium.solo.Solo;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class TestProfileFragment {
-  /*private Solo solo;
+  private Solo solo;
 
   @Rule
   public ActivityTestRule<GameActivity> rule = new ActivityTestRule(GameActivity.class, true, true);
@@ -15,12 +23,12 @@ public class TestProfileFragment {
   @Before
   public void setUp() throws Exception {
     solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-    solo.clickOnView(solo.getView(R.id.navigation_profile));
+    solo.clickOnView(solo.getView(R.id.navigation_my_profile));
   }
 
   @Test
   public void testStartProfile() {
     GameController gc = ((GameActivity) solo.getCurrentActivity()).getController();
     assertTrue(gc.getBody() instanceof ProfileFragment);
-  }*/
+  }
 }
---
+++ app/src/test/java/com/cmput301w23t09/qrhunter/qrcode/TestQRModel.java
@@ -0,0 +1,55 @@
+package com.cmput301w23t09.qrhunter.qrcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+
+public class TestQRModel {
+    // create a mock hash
+    private String mockHash() {
+        return "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    }
+    // create a mock qr code
+    private QRCode mockCode() {
+        return new QRCode(mockHash());
+    }
+    // create a mock qr code list
+    private ArrayList<QRCode> mockList() {
+        return new ArrayList<QRCode>();
+    }
+
+    @Test
+    public void testGetHash() {
+        assertEquals(mockCode().getHash(), mockHash());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals(mockCode().getName(), "");
+    }
+
+    @Test
+    public void testGetScore() {
+        assertEquals((int) mockCode().getScore(), 0);
+    }
+
+    @Test
+    public void testGetLoc() {
+        assertEquals(mockCode().getLoc(), null);
+    }
+
+    @Test
+    public void testQRSort() {
+        ArrayList<QRCode> mockList = mockList();
+        mockList.add(mockCode());
+        // add another qr code with a higher score
+        Comparator<QRCode> comparator = new ScoreComparator();
+        mockList.sort(comparator);
+        // check if order is correct
+        // check with comparator.reversed()
+    }
+}
