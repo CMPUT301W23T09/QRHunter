0543d3dc01fef43276212377563760e05895656a
JordanLa12 <jpla@ualberta.ca>
2023-03-12 22:02:00 -0600

    Add google places map functionality and remove old google maps

--- app/src/main/java/com/cmput301w23t09/qrhunter/navigation/NavigationControllerAdapter.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/navigation/NavigationControllerAdapter.java
@@ -3,7 +3,7 @@ package com.cmput301w23t09.qrhunter.navigation;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import com.cmput301w23t09.qrhunter.GameController;
-import com.cmput301w23t09.qrhunter.MapsFragment;
+import com.cmput301w23t09.qrhunter.map.MapFragment;
 import com.cmput301w23t09.qrhunter.R;
 import com.cmput301w23t09.qrhunter.profile.ProfileFragment;
 import com.cmput301w23t09.qrhunter.scanqr.ScannerFragment;
@@ -31,7 +31,7 @@ public class NavigationControllerAdapter implements NavigationBarView.OnItemSele
       gameController.setBody(new ProfileFragment(gameController, deviceUUID));
     } else if (selectedItemId == R.id.navigation_qr_finder) {
       // mainController.setBody(new QRFinderFragment());
-      gameController.setBody(new MapsFragment());
+//      gameController.setBody(new MapFragment());
     } else if (selectedItemId == R.id.navigation_social) {
       // mainController.setBody(new SocialFragment());
     } else {
