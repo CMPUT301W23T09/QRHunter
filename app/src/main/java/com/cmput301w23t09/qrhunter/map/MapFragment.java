c9165885f246cf0a8a8bb9a13c68fdeef275a3ee
JordanLa12 <jpla@ualberta.ca>
2023-03-13 02:11:58 -0600

    Add javaDoc to methods and minor fixes

--- app/src/main/java/com/cmput301w23t09/qrhunter/map/MapFragment.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/map/MapFragment.java
@@ -1,6 +1,5 @@
 package com.cmput301w23t09.qrhunter.map;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -23,7 +22,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -35,27 +36,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private Location lastKnownLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private static final String TAG = "MapFragment";
-    private  static final int DEFAULT_ZOOM = 10;
-    private LatLng defaultLocation = new LatLng(-34, 151);
+    private  static final int DEFAULT_ZOOM = 15;
+    private LatLng defaultLocation = new LatLng(-34, -34);
 
 
 
     public MapFragment() {
         // Required empty public constructor
     }
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-////        // Initialize the SDK
-////        Places.initialize(getContext().getApplicationContext(), "AIzaSyDniTKVk4HDVsQVG-uDxQ-eFV4nCWeM-gU");
-////
-////        // Create a new PlacesClient instance
-////        PlacesClient placesClient = Places.createClient(getContext());
-////
-////        // Construct a FusedLocationProviderClient.
-////        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
-//    }
 
     private void getLocationPermission() {
         /**
@@ -76,43 +64,50 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void getDeviceLocation() {
-        // Create an instance of FusedLocationProviderClient
-        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        /**
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
 
-        // Check for location permission
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Request location permission if not granted
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            return;
-        }
+//        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
 
-        // Get the last known location
-        Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
-        locationResult.addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                if (task.isSuccessful()) {
-                    // Set the map's camera position to the current location of the device.
-                    lastKnownLocation = task.getResult();
-                    if (lastKnownLocation != null) {
-                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-//                        // Call nearby search method to show nearby places
-//                        nearbySearch(new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()));
-                    } else {
-                        // If current location is null, show default location
-                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
-                        map.getUiSettings().setMyLocationButtonEnabled(false);
+        try {
+            if (locationPermissionGranted) {
+                //Get the last known location
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                //Todo Add map.addMarker()
+
+                            }
+                        } else {
+                            // Sets the map camera to the a set default location if lastKnownLocation is null
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.animateCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
                     }
-                } else {
-                    // If location task fails, show default location
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
-                    map.getUiSettings().setMyLocationButtonEnabled(false);
-                }
+                });
             }
-        });
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
     }
 
 
+    /**
+     * Updates the UI depending on the whether location permissions
+     * are granted by the user
+     */
     private void updateLocationUI() {
         if (map == null) {
             return;
@@ -122,6 +117,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 map.setMyLocationEnabled(true);
                 map.getUiSettings().setMyLocationButtonEnabled(true);
             } else {
+                // Don't get the location and instead ask for location permissions
                 map.setMyLocationEnabled(false);
                 map.getUiSettings().setMyLocationButtonEnabled(false);
                 lastKnownLocation = null;
@@ -132,6 +128,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         }
     }
 
+    /**
+     * Handles the users response to permission dialogue box popup
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -149,38 +148,50 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         updateLocationUI();
     }
 
-
-
+    /**
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return View the view which is inflated displaying the the R.layout.map xml file
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Places.initialize(getContext().getApplicationContext(), "AIzaSyDniTKVk4HDVsQVG-uDxQ-eFV4nCWeM-gU");
-
-        // Create a new PlacesClient instance
-        PlacesClient placesClient = Places.createClient(getContext());
-
-        // Construct a FusedLocationProviderClient.
-        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
-        // Inflate the layout for this fragment
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
+    /**
+     *
+     * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        //Checks if support map fragment is found, if so pass current fragment as callback
         if (mapFragment != null) {
+            // Gets a googleMap object
             mapFragment.getMapAsync(this);
         }
     }
 
+    /**
+     *
+     * @param map the Google maps
+     */
     @Override
     public void onMapReady(GoogleMap map) {
-        this.map = map;
-
-        // ...
-
         // Turn on the My Location layer and the related control on the map.
         updateLocationUI();
 
@@ -188,8 +199,4 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         getDeviceLocation();
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
 }
