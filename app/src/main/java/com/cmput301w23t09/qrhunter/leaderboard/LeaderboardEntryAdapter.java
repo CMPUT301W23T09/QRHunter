7fe4bffb4fc70937e7555453ebf40d137ebd73f9
John Mabanta <johnmabanta51@gmail.com>
2023-03-31 15:59:19 -0600

    Added leaderboard pictures

--- app/src/main/java/com/cmput301w23t09/qrhunter/leaderboard/LeaderboardEntryAdapter.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/leaderboard/LeaderboardEntryAdapter.java
@@ -1,15 +1,19 @@
 package com.cmput301w23t09.qrhunter.leaderboard;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.cmput301w23t09.qrhunter.R;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /** ArrayAdapter for leaderboard entries */
 public class LeaderboardEntryAdapter extends ArrayAdapter<LeaderboardEntry> {
@@ -41,6 +45,22 @@ public class LeaderboardEntryAdapter extends ArrayAdapter<LeaderboardEntry> {
 
     TextView score = view.findViewById(R.id.leaderboard_entry_score);
     score.setText(entry.getScore() + " " + entry.getScoreSuffix());
+
+    ImageView picture = view.findViewById(R.id.leaderboard_entry_pic);
+    try {
+      if (entry instanceof QRCodeLeaderboardEntry) {
+        picture.setImageBitmap(
+            ((QRCodeLeaderboardEntry) entry).getQRCode().getVisualRepresentation());
+      } else if (entry instanceof PlayerLeaderboardEntry) {
+        picture.setImageBitmap(((PlayerLeaderboardEntry) entry).getPlayer().getProfilePic());
+      } else {
+        throw new IllegalArgumentException("Invalid Leaderboard entry type!");
+      }
+    } catch (ExecutionException | InterruptedException e) {
+      Log.e("LeaderboardEntryAdapter", "An exception occurred while fetching image", e);
+      Toast.makeText(context, "An exception occurred while fetching image...", Toast.LENGTH_LONG)
+          .show();
+    }
 
     return view;
   }
