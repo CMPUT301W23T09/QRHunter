f56ae1f247f16c05ce53e4ee9502af5a7e716c13
John Mabanta <johnmabanta51@gmail.com>
2023-04-03 00:19:01 -0600

    Location Photo slider now shows on QRCodeFragment

--- app/src/main/java/com/cmput301w23t09/qrhunter/qrcode/AddQRCodeFragment.java
+++ app/src/main/java/com/cmput301w23t09/qrhunter/qrcode/AddQRCodeFragment.java
@@ -81,7 +81,6 @@ public class AddQRCodeFragment extends QRCodeFragment {
 
     updateAddButton();
     addButton.setOnClickListener(this::onAddQRClicked);
-    updateLocationPhoto();
   }
 
   @Override
