8997f629005b0d8b463dfd337d785cd7bdef583b
andy-mtng <104605803+andy-mtng@users.noreply.github.com>
2023-04-02 19:14:25 -0600

    Search Functionality (#69)
    * Add inital search elements
    
    * Fix some documentation
    
    * Add gameController to LeaderboardFragment and add search page transition
    
    * Clean up PlayerSearchFragment code
    
    * Start back button implementation
    
    * Finish back button implementation
    
    * Clean up QRCodeVisualFetcher.java
    
    * Set up files for adapter
    
    * Add listView to player search fragment
    
    * Set up SearchQueryEntry, SearchQueryEntryAdapter, and UI files
    
    * Get search query list working with test data
    
    * Refactor PlayerSearchController and move methods to it
    
    * Add initial implementation of getPlayersWithRelatedUsernames
    
    * Remove Firebase related substring query. Replace with set filtering
    
    * Add functionality to display related usernames in search
    
    * Set up skeleton for transition to other player profiles
    
    * Fix lowercase bug
    
    * Add Player Not Found message to search
    
    * Refactor displaySearchQueryData
    
    * Clean up comments
    
    * Connect search to other profiles
    
    * Remove search query textView from search fragment
    
    * Write test to check if search leads to fragment transition
    
    * Write test to check for no players
    
    * Test search back button
    
    * Finish related usernames test for search queries
    
    * Finish writing tests for search
    
    * Finish javadocs for PlayerSearchController
    
    * Finish javadocs for SearchQueryEntry
    
    * Finish Javadocs for SearchQueryEntryAdapter
    
    * Finish javadocs for the PlayerSearchFragment
    
    * Use spotlessApply
    
    * Fix testRelatedUsernameSearch
    
    * Fix fragment_leaderboard.xml
    
    * Run spotlessApply
    
    * Add citations
    
    * Run spotlessApply
    
    * Add profile pictures to search queries
    
    * Adjust related usernames test
    
    * use closeSoftKeyboard
    
    * use search_src_text
    
    * wait until any data exists in list view
    
    * Make minor javadoc adjustments
    
    ---------
    
    Co-authored-by: WillQi <williamqipizza@gmail.com>

---
+++ app/src/main/java/com/cmput301w23t09/qrhunter/leaderboard/SearchQueryEntryAdapter.java
@@ -0,0 +1,71 @@
+package com.cmput301w23t09.qrhunter.leaderboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.cmput301w23t09.qrhunter.R;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * ArrayAdapter for search query entries
+ *
+ * @author Andy Nguyen
+ * @version 1.0
+ */
+public class SearchQueryEntryAdapter extends ArrayAdapter<SearchQueryEntry> {
+  /** Search query entries to use for this adapter */
+  private final List<SearchQueryEntry> entries;
+  /** Context for the search fragment */
+  private final Context context;
+
+  /**
+   * Constructor for the SearchQueryEntryAdapter
+   *
+   * @param context Context for the saerch fragment
+   * @param entries Search query entries to use for this adapter
+   */
+  public SearchQueryEntryAdapter(Context context, List<SearchQueryEntry> entries) {
+    super(context, 0, entries);
+    this.entries = entries;
+    this.context = context;
+  }
+
+  /**
+   * Gets the view that represents the search query entry at a certain position in the list
+   *
+   * @param position Position of the search entry
+   * @param convertView Potential view the adapter can reuse
+   * @param parent Parent of convertView
+   * @return View that represents the search query entry
+   */
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    View view = convertView;
+    if (view == null) {
+      view = LayoutInflater.from(context).inflate(R.layout.search_query_entry_view, parent, false);
+    }
+
+    SearchQueryEntry entry = entries.get(position);
+
+    // set fields of view
+    TextView name = view.findViewById(R.id.search_query_entry_text);
+    name.setText(entry.getName());
+    ImageView picture = view.findViewById(R.id.search_query_image);
+    try {
+      picture.setImageBitmap(entry.getPlayer().getProfilePic());
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+
+    return view;
+  }
+}
